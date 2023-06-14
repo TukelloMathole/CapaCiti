d41f4e180c16718b44d25b07025c1eb1a526d9d4
Tukello Cristian <105870343+TukelloMathole@users.noreply.github.com>
2023-06-14 10:49:25 +0200

    Add files via upload

---
+++ school Website/faqs.php
@@ -0,0 +1,47 @@
+<?php
+include 'inc/header.php';
+?>
+
+<div class="FAQcontainer">
+    <div class="maincontainer">
+        <div class="theCard">
+            <div class="theFront">
+                <h3>What grades does the school offer?</h3>
+            </div>
+            <div class="theBack">We offer grade R to grade 7.</div>
+        </div>
+    </div>
+    <div class="maincontainer">
+        <div class="theCard">
+            <div class="theFront">
+                <h3>When do school applications open?</h3>
+            </div>
+            <div class="theBack">School applications open every year in March. Check the WCED website for actual dates.</div>
+        </div>
+    </div>
+    <div class="maincontainer">
+        <div class="theCard">
+            <div class="theFront">
+                <h3>How can I report the childs absence from school?</h3></div>
+            <div class="theBack">You can contact the school through the contact number or communicate with the childs class teacher.</div>
+        </div>
+    </div>
+    <div class="maincontainer">
+        <div class="theCard">
+            <div class="theFront">
+                <h3>Where can I buy the school uniform? </h3></div>
+            <div class="theBack">The school uniform can be purchased at the school. Ask help at the reception.</div>
+        </div>
+    </div>
+    <div class="maincontainer">
+        <div class="theCard">
+            <div class="theFront">
+                <h3>What do I do if my child has medication prescription that they have to take during school hours?</h3>  
+            </div>
+            <div class="theBack">You can direct your quiry to the childs class teacher, to find common grounds.</div>
+        </div>
+    </div>
+</div>
+<?php
+include 'inc/footer.php';
+?>
