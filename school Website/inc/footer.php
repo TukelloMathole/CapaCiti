7a79ef0e3cce92cf64ea90c406fafc78f147691f
Tukello Cristian <105870343+TukelloMathole@users.noreply.github.com>
2023-06-14 10:51:51 +0200

    Add files via upload

---
+++ school Website/inc/footer.php
@@ -0,0 +1,39 @@
+<div class="footer">
+    <div class="items" id="adminLogin">
+
+        <a href="./Admin_pages/login.php"><i class="bi bi-person-circle"></i>Login</a>
+    </div>
+    <div class="items">
+        <ul>
+            <li><a href="/home.php">Home</a></li>
+            <li><a href="/about.php">About</a></li>
+            <li><a href="/contactus.php">Contact</a></li>
+            <li><a href="/events.php">Events</a></li>
+            <li><a href="/faqs.php">FAQs</a></li>
+        </ul>
+    </div>
+    <div class="items" id="social">
+        <i class="bi bi-facebook"></i>
+        <i class="bi bi-whatsapp"></i>
+        <i class="bi bi-linkedin"></i>
+        <i class="bi bi-twitter"></i>
+    </div>
+    <div class="items">
+        <ul>
+            <li><a href="/Careers.php">Careers</a></li>
+            <li><a href="/Donations.php">Donations</a></li>
+            <li><a href="/Sponsors.php">Sponsors</a></li>
+            <li><a href="/staff.php">Staff</a></li>
+        </ul>
+    </div>
+</div>
+</body>
+<script src="./javascript/script_application.js"></script>
+<script src="./javascript/navigation.js"></script>
+<script src="./javascript/contactus.js"></script>
+<script src="./javascript/form.js"></script>
+<script src="./javascript/faqs.js"></script>
+
+
+
+</html>
---
+++ school Website/inc/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="./Css/style.css">
+    <link rel="stylesheet" href="./Css/navigation.css">
+    <link rel="stylesheet" href="./Css/index.css">
+    <link rel="stylesheet" href="/Css/about.css">
+    <link rel="stylesheet" href="./Css/contact.css">
+    <link rel="stylesheet" href="./Css/events.css">
+    <link rel="stylesheet" href="./Css/footer.css">
+    <link rel="stylesheet" href="./Css/faqs.css">
+    
+
+    <!--bootstrap links-->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css" />
+    
+    <!--bootstrap links-->
+    <title></title>
+</head>
+
+<body>
+    <div class="navigation">
+        <div class="logo"><img src="./images/logo.png"></div>
+        <div id='myDIV' class="links">
+            <ul>
+                <li><a href="/index.php" style="color:black; text-decoration: none;">Home</a></li>
+                <li><a href="/about.php" style="color:black; text-decoration: none;">About</a></li>
+                <li><a href="/contactus.php" style="color:black; text-decoration: none;">Contact</a></li>
+                <li><a href="/events.php" style="color:black; text-decoration: none;">Events</a></li>
+                <li><a href="/faqs.php" style="color:black; text-decoration: none;">FAQs</a></li>
+            </ul>
+        </div>
+        <div class="mediaButton">
+            <button class="mainMediaButton" onclick="show()">
+                <span></span>
+                <span></span>
+                <span></span>
+            </button>
+        </div>
+    </div>
